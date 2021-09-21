76e4cf169df4469d78305f7434c2af19aa615ce5
william.roberto <william.roberto@healthx.com.br>
2021-09-21 09:12:00 -0300

    atualização

--- assistenteVirtualIFPE/assistenteVirtualIFPETestAvatar.py
+++ assistenteVirtualIFPE/assistenteVirtualIFPETestAvatar.py
@@ -3,16 +3,25 @@ import time
 import cv2
 import os
 
-DirPath = "assistenteVirtualIFPE\\assistenteVirtualIFPEAvatarModelo\\"
+diretorio1 = "assistenteVirtualIFPE\\assistenteVirtualIFPEAvatarModelo\\a.png"
 
-Files = os.listdir(DirPath)
+diretorio2 = "assistenteVirtualIFPE\\assistenteVirtualIFPEAvatarModelo\\b.png"
 
-for File in Files:
-    imgPath = os.path.join(DirPath, File)
-    print(imgPath)
-    image = cv2.imread(imgPath)
-    cv2.imshow("image", image)
-    # time.sleep(1)
-    # cv2.destroyAllWindows()
-    cv2.waitKey(0)
-cv2.destroyAllWindows()
+def tarefa1():
+    for i in range(10):
+        image = cv2.imread(diretorio1)
+        time.sleep(1)
+        cv2.imshow("image", image)
+        time.sleep(1)
+        cv2.destroyAllWindows()
+
+def tarefa2():
+    for i in range(10):
+        image = cv2.imread(diretorio2)
+        time.sleep(1)
+        cv2.imshow("image", image)
+        time.sleep(1)
+        cv2.destroyAllWindows()
+
+threading.Thread(target=tarefa1).start()
+tarefa2()
